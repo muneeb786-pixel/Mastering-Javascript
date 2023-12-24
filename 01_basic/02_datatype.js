5cc70f228563e990406dbaa8091d1c0e42de3e05
Muneeb ur rehman <62784910+muneeb786-pixel@users.noreply.github.com>
2023-12-24 10:30:01 +0000

    basic datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,31 @@
+// in this we will look for basic datatypes not in deep will see null and undefined 
+"use strict"  // this mean treat all JS as a new version 
+
+// we can't use window element as we mostly do in our browsers. like
+// alert("hello")
+
+let name="Muneeb ur rehman" //string
+let age= 22 //number
+let isAdmin=true //boolean 
+
+/*
+here are some important datatype
+number => 2 to power 52
+string => ""
+boolean => true/false
+bigInt 
+null => standalone value 
+undefined => undefined like let state; so this is not null it will be undefined 
+
+symbol =>unique we will see in detail further 
+object
+
+*/
+
+// check type of variable or value 
+
+console.log(typeof null);
+// its type is object 
+
+console.log(typeof undefined)
+// its type is undefined
