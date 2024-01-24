1c1805c9e652f98eb1a234b3064c24191f34b613
Muneeb ur rehman <62784910+munib-ehman@users.noreply.github.com>
2024-01-24 15:48:58 +0000

    numbers

---
+++ 01_basic/number.js
@@ -0,0 +1,12 @@
+// numbers 
+
+const score = 1000;
+console.log(score.toLocaleString());
+
+// fixed and round
+
+const number = 100.93
+
+console.log(number.toFixed());
+
+console.log(number.toPrecision(4));
