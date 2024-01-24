084882bcbb3205a2782e6d8e75c524d0ecb1b603
Muneeb ur rehman <62784910+munib-ehman@users.noreply.github.com>
2024-01-24 16:54:18 +0000

    datre and time

---
+++ 01_basic/date.js
@@ -0,0 +1,10 @@
+const date = new Date()
+
+console.log(date.getTime())
+
+console.log(date.toLocaleString('default',{
+    dateStyle:"full",
+}));
+
+const myDate = new Date("2024-1-31")
+console.log(myDate);
